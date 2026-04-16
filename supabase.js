e78d10ddd0d9b38a7a5ebf51e58833f4404a4bc2
canostboI <canostboi@gmail.com>
2026-04-16 10:20:01 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
 
+const SUPABASE_URL = "https://istrnsicleopzbsrapsw.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_U_bWR5hSNYslkj4A0XPdJA_m2GMWInY";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+window.supabaseClient = supabase;
